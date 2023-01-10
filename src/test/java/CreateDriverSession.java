7b93714ae8b000f93ce2414e6923b12bcb7dc217
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2023-01-10 21:31:58 +0300

    Creating Driver Session

---
+++ src/test/java/CreateDriverSession.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class CreateDriverSession {
+
+    public static void main(String[] args) {
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+
+    }
+}
